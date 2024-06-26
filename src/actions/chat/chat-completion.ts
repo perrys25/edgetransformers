7802cec18e4c41649639a380eb09d3956575c9c2
Perry Sahnow <57735040+perrys25@users.noreply.github.com>
2024-06-26 14:05:48 -0400

    Chat Completion Function

---
+++ src/actions/chat/chat-completion.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { RoleScopedChatInput } from "@cloudflare/workers-types/2023-07-01/index";
+
+export async function chatCompletion(
+  model: BaseAiTextGenerationModels,
+  prompt: RoleScopedChatInput[],
+) {
+  const context = getRequestContext();
+
+  const response = (
+    (await context.env.AI.run(model ?? "@hf/google/gemma-7b-it", {
+      messages: prompt,
+    })) as { response: string }
+  ).response;
+
+  return response;
+}
